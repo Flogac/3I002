3fc72d4923446b02aff2974be3324da1a5d5b0d2
Flogac <florentgacougnolle@yahoo.fr>
2017-01-17 02:18:12 +0100

    Fin+ajout tests

--- src/pobj/motx/tme1/GrilleMots.java
+++ src/pobj/motx/tme1/GrilleMots.java
@@ -7,34 +7,45 @@ public class GrilleMots {
 
 	private List<Mot> mots;
 	Grille leGrill;
+	int nbHorizontal = 0;
+	int nbVertical = 0;
 	
 	public GrilleMots (Grille grille){
 		leGrill = grille;
-		
+		for( int i = 0 ; i < leGrill.nbCol() ; i++ ){
+			chercheMots(getCol(i));
+		}
+		nbVertical = mots.size();
+		for( int i = 0 ; i < leGrill.nbLig() ; i++ ){
+			chercheMots(getLig(i));
+		}
+		nbHorizontal = mots.size() - nbVertical;
 	}
 	
 	public List<Mot> getMots(){
-		
+		return mots;
 	}
 	
 	public int getNbHorizontal(){
-		
+		return nbHorizontal;
 	}
 	
-	public int getNbHorizontal(){
-		
+	public int getNbVertical(){
+		return nbVertical;
 	}
 	
 	private List<Case> getLig (int lig){
 		List<Case> retour = new ArrayList<Case>();
 		for( int i = 0 ; i < leGrill.nbCol() ; i++ )
 			retour.add( leGrill.getCase(lig, i));
+		return retour;
 	}
 	
 	private List<Case> getCol (int col){
 		List<Case> retour = new ArrayList<Case>();
 		for( int i = 0 ; i < leGrill.nbLig() ; i++ )
 			retour.add( leGrill.getCase(i, col));
+		return retour;
 	}
 	
 	private void chercheMots(List<Case> cases) {
@@ -52,5 +63,11 @@ public class GrilleMots {
 				retour.clear();
 			}
 		}
+	}
+	
+	public String toString(){
+		StringBuilder retour = new StringBuilder();
+		for( int i = 0 ; i < mots.size() ; i++ ) retour.append( mots.get(i).toString() + '\n');
+		return retour.toString();
 	}
 }
